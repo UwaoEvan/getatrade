ca334aeff7b738d596870f2ee5cae87d6909dbdc
UwaoEvan <uwaoevanz@gmail.com>
2025-04-28 11:02:21 +0300

    forced the layout to be dynamic so that the cookies get refreshed

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export const metadata: Metadata = {
   description: "One stop shop",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({
   children,
 }: Readonly<{
