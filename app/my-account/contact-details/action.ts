cdee7563c5d980866a76fe8a23bb850b20abd81f
UwaoEvan <uwaoevanz@gmail.com>
2025-06-26 19:09:37 +0300

    fixed the edit contacts

---
+++ app/my-account/contact-details/action.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { State } from "@/app/lib/actions";
+import { db } from "@/app/lib/db";
+
+export const updateContactDetails = async (
+  prevState: State,
+  formData: FormData,
+) => {
+  const userId = formData.get("userId") as string;
+
+  const user = await db.user.update({
+    where: {
+      id: parseInt(userId),
+    },
+    data: {
+      firstName: formData.get("firstName") as string,
+      lastName: formData.get("lastName") as string,
+      address: formData.get("address") as string,
+      location: formData.get("location") as string,
+    },
+  });
+
+  if (!user) {
+    return {
+      error: "User not found",
+      success: false,
+    };
+  }
+  return {
+    success: true,
+  };
+};
--- app/my-account/contact-details/page.tsx
+++ app/my-account/contact-details/page.tsx
@@ -1,60 +1,188 @@
+"use client";
+
+import Location from "@/app/components/Location";
 import { getUser } from "@/app/lib/actions";
 import { auth } from "@/app/lib/auth";
-import React from "react";
+import React, { Suspense, useActionState, useEffect, useState } from "react";
+import { updateContactDetails } from "./action";
 
-const ContactDetails = async () => {
-  const session = await auth();
-  const user = await getUser(session?.user?.email as string);
+const initState = { error: undefined, success: false };
+const ContactDetails = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [user, setUser] = useState<any>(null);
+  const [location, setLocation] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [state, formAction, isPending] = useActionState(
+    updateContactDetails,
+    initState,
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const session = await auth();
+      const userData = await getUser(session?.user?.email as string);
+      setUser(userData);
+    };
+    setIsEditing(false);
+    fetchData();
+  }, [state]);
+
+  const handleEditToggle = () => setIsEditing(!isEditing);
+
   return (
-    <div className="max-w-md mx-auto md:p-6 space-y-4">
-      <h2 className="text-xl md:text-2xl font-bold text-gray-800">
-        Contact Details
-      </h2>
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-[80vh]">
+          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-[#2f76d9]"></div>
+        </div>
+      }
+    >
+      <div className="max-w-md mx-auto md:p-6 space-y-4">
+        <h2 className="text-xl md:text-2xl font-bold text-gray-800">
+          Contact Details
+        </h2>
 
-      {user?.role === "customer" ? (
-        <>
-          <div>
-            <p className="text-sm text-gray-500">Name</p>
-            <p className="text-md text-gray-700 font-medium">{user.username}</p>
-          </div>
+        {!isEditing ? (
+          <>
+            {user?.role === "customer" ? (
+              <>
+                <div>
+                  <p className="text-sm text-gray-500">Name</p>
+                  <p className="text-md text-gray-700 font-medium">
+                    {user.username}
+                  </p>
+                </div>
 
-          <div>
-            <p className="text-sm text-gray-500">Email</p>
-            <p className="text-md text-gray-700 font-medium">{user.email}</p>
-          </div>
+                <div>
+                  <p className="text-sm text-gray-500">Email</p>
+                  <p className="text-md text-gray-700 font-medium">
+                    {user.email}
+                  </p>
+                </div>
 
-          <div>
-            <p className="text-sm text-gray-500">Phone Number</p>
-            <p className="text-md text-gray-700 font-medium">
-              {user?.phoneNumber}
-            </p>
-          </div>
-        </>
-      ) : (
-        <>
-          <div>
-            <p className="text-sm text-gray-500">Trading name</p>
-            <p className="text-md text-gray-700 font-medium">
-              {user?.username}
-            </p>
-          </div>
+                <div>
+                  <p className="text-sm text-gray-500">Phone Number</p>
+                  <p className="text-md text-gray-700 font-medium">
+                    {user?.phoneNumber}
+                  </p>
+                </div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <p className="text-sm text-gray-500">Trading name</p>
+                  <p className="text-md text-gray-700 font-medium">
+                    {user?.username}
+                  </p>
+                </div>
 
-          <div>
-            <p className="text-sm text-gray-500">Name</p>
-            <p className="text-md text-gray-700 font-medium">
-              {user?.firstName}
-            </p>
-          </div>
+                <div>
+                  <p className="text-sm text-gray-500">Name</p>
+                  <p className="text-md text-gray-700 font-medium">
+                    {user?.firstName}
+                  </p>
+                </div>
 
-          <div>
-            <p className="text-sm text-gray-500">Address</p>
-            <p className="text-md text-gray-700 font-medium">
-              {user?.location}
-            </p>
-          </div>
-        </>
-      )}
-    </div>
+                <div>
+                  <p className="text-sm text-gray-500">Address</p>
+                  <p className="text-md text-gray-700 font-medium">
+                    {user?.address}
+                  </p>
+                </div>
+
+                <div>
+                  <p className="text-sm text-gray-500">Location</p>
+                  <p className="text-md text-gray-700 font-medium">
+                    {user?.location}
+                  </p>
+                </div>
+              </>
+            )}
+
+            <button
+              onClick={handleEditToggle}
+              className="mt-4 px-4 py-2 bg-blue-600 text-white rounded"
+            >
+              Edit
+            </button>
+          </>
+        ) : (
+          <form action={formAction} className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                First name *
+              </label>
+              <input
+                type="text"
+                defaultValue={user?.firstName}
+                name="firstName"
+                className="mt-1 block w-full rounded-sm border-gray-300 shadow-sm focus:ring-[#2f76d9] focus:border-[#2f76d9] sm:text-sm p-3"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Last name *
+              </label>
+              <input
+                type="text"
+                defaultValue={user?.lastName}
+                name="lastName"
+                className="mt-1 block w-full rounded-sm border-gray-300 shadow-sm focus:ring-[#2f76d9] focus:border-[#2f76d9] sm:text-sm p-3"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Address *
+              </label>
+              <input
+                type="text"
+                name="address"
+                defaultValue={user?.address}
+                className="mt-1 block w-full rounded-sm border-gray-300 shadow-sm focus:ring-[#2f76d9] focus:border-[#2f76d9] sm:text-sm p-3"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Location *
+              </label>
+              <div>
+                <Location
+                  value={location || user?.location}
+                  onChange={setLocation}
+                  placeholder="Start typing your location..."
+                  className="mt-1 block w-full rounded-sm border-gray-300 shadow-sm focus:ring-[#2f76d9] focus:border-[#2f76d9] sm:text-sm p-3"
+                />
+              </div>
+              <input
+                type="hidden"
+                name="location"
+                value={location || user?.location}
+              />
+              <input type="hidden" name="userId" value={user?.id} />
+            </div>
+
+            <div className="flex gap-2">
+              <button
+                type="button"
+                onClick={() => setIsEditing(false)}
+                className="px-4 py-2 border rounded text-blue-600 border-blue-600 hover:bg-purple-50"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+              >
+                {isPending ? "Saving..." : "Save"}
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
+    </Suspense>
   );
 };
 
