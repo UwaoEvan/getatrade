9dc63eecedd54564e838d597337237f9a6d1d20a
UwaoEvan <uwaoevanz@gmail.com>
2025-07-05 14:09:34 +0300

    fixed the interested api

--- app/api/mobile-app/activity/interested/route.ts
+++ app/api/mobile-app/activity/interested/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
     }
 
     const interested = await db.interest.findMany({
-      where: { userId: user.userId },
+      where: { userId: user.userId, shortlisted: false },
       include: {
         job: true,
       },
